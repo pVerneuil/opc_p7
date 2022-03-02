92676db4933c78604106b570fcc5281b92be5b1d
Pverneuil <verneuil.paul@gmail.com>
2022-03-02 19:58:42 +0100

    feat: brute force and optimide algo working

--- brute_force.py
+++ brute_force.py
@@ -1,21 +1,5 @@
 import copy
-import time
-
-def calc_profit_and_cost_of_combination(combination):
-    """calculate profit and costs of a combination of action
-
-    Args:
-        combination (list): action in the format [ id , cost, margin]
-    Returns:
-    [cost, profit]
-    """
-    cost = 0
-    profit = 0
-    for action in combination:
-        cost += action[1]
-        profit += action[1]*action[2]/100
-    return[cost, profit]
-
+from utilities import *
 
 def find_all_subset(lst):
     list = copy.copy(lst)
@@ -33,32 +17,9 @@ def find_all_subset(lst):
     actual_function(list, subset)
     return subsets
 
-raw = [
-    [1, 20, 5],
-    [2, 30, 10],
-    [3, 50, 15],
-    [4, 70, 20],
-    [5, 60, 17],
-    [6, 80, 25],
-    [7, 22, 7],
-    [8, 26, 11],
-    [9, 48, 13],
-    [10, 34, 27],
-    [11, 42, 17],
-    [12, 110, 9],
-    [13, 38, 23],
-    [14, 14, 1],
-    [15, 18, 3],
-    [16, 8, 8],
-    [17, 4, 12],
-    [18, 10, 14],
-    [19, 24, 21],
-    [20, 114, 18]]
 
-
+@get_time
 def first_iteration(list): #O(2^n); ~3s 
-
-    start_time = time.time()
 
     data = find_all_subset(list)
     valid = []
@@ -69,10 +30,5 @@ def first_iteration(list): #O(2^n); ~3s
         if cost <= 500:
             valid.append([subset, cost,profit])
     valid.sort(key=lambda x: x[2], reverse=True)
-    print (len(valid))
-    print(valid[0])
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-first_iteration(raw)
-
+    return(valid[0])
 
---
+++ optimized.py
@@ -0,0 +1,39 @@
+import numpy as np
+from utilities import get_time
+
+
+@get_time
+def knapsack(actions, max_cost):
+    
+    matrix = np.zeros( (len(actions) + 1,max_cost + 1))
+    for i in range(len(actions) + 1):
+        for j in range(max_cost + 1):
+            if i == 0 or j == 0:
+                matrix[i,j] = 0
+            elif actions[i - 1].price <= j:
+                matrix[i,j] = max(
+                    actions[i - 1].profit + matrix[i - 1,j - int(actions[i - 1].price)], 
+                    matrix[i - 1, j]
+                )
+            else:
+                matrix[i, j] = matrix[i - 1, j]
+                
+    best_profit = matrix[len(actions),max_cost]
+    
+    wallet = []
+    w = max_cost
+    
+    for i in range(len(actions), 0, -1):
+        if best_profit <= 0:
+            break
+        if best_profit == matrix[i - 1, int(w)]:
+            continue
+        else:
+            best_profit = best_profit - actions[i - 1].profit
+            w = w - actions[i - 1].price
+            wallet.append(actions[i - 1].name)
+
+
+    return {
+        "profit": matrix[len(actions),max_cost],
+        "wallet": wallet}
